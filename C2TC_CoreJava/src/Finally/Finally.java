dd94b055662e2f94fff32732ea3633622c2010bc
Admin <Admin@DESKTOP-5LFPQ1I>
2022-06-14 20:41:54 +0530

    finally

---
+++ C2TC_CoreJava/src/Finally/Finally.java
@@ -0,0 +1,22 @@
+
+package Finally;
+
+public class Finally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int d=100/0;
+			System.out.println(d);
+		}
+catch(Exception e)
+		{
+	System.out.println("can't divide integer by 0");
+		}
+		finally {
+			System.out.println("the try catch is finished");
+		}
+	}
+
+}
+
---
+++ corejava/src/finnaly/finnaly.java
@@ -0,0 +1,21 @@
+package finnaly;
+
+public class finnaly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int d=100/0;
+			System.out.println(d);
+		}
+catch(Exception e)
+		{
+	System.out.println("can't divide integer by 0");
+		}
+		finally {
+			System.out.println("the try catch is finished");
+		}
+	}
+
+}
+
