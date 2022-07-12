99d2b1c1f71685398fda17ab0259a8d6e5ae19dd
Admin <Admin@DESKTOP-5LFPQ1I>
2022-07-12 20:40:35 +0530

    Concatenation

---
+++ C2TC_CoreJava/src/Concatenation/Concat.java
@@ -0,0 +1,13 @@
+package Concatenation;
+
+public class Concat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String S="hello";
+		S.concat("world");
+		System.out.println(S);
+
+	}
+
+}
